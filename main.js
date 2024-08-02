acbc369e8b3e553f09a11a74513f47ea6acda4a0
jmak-3 <152338375+jmak-3@users.noreply.github.com>
2024-08-02 18:02:06 +0100

    Upload my files to add contact and about animation

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
     let button = document.querySelector('button');
     let coup = document.querySelector('.attempt');
     var n = 5;
-    coup.innerHTML = 'Attempt : ' + n;
+    coup.innerText = 'Attempt : ' + n;
     let quest = document.querySelector('.equation');
-    quest.innerHTML = "4x - (√256) = 12";
+    quest.innerText = "4x - (√256) = 12";
+    
+    
+  function animated() {
+    let el1 = document.querySelector('item1');
+    let el2 = document.querySelector('item2');
+    el1.style.display = 'flex';
+    el2.style.display = 'flex';
+  }
+    
     
     function getTest() {
-      let el = document.getElementById('input').value;
+      let el = parseInt(document.getElementById('input').value);
       let mes = document.querySelector('.reponse');
-      let rep = '7';
+      let rep = 7;
       while (n > 0) {
         if (el != rep) {
           n --;
@@ -17,16 +26,14 @@
           if (n == 0) {
             mes.innerHTML = 'Vous avez perdu ! Améliorez votre QI';
             button.style.display = 'none';
-            break
           }else {
-          break
+          break;
           }
         } else if (el === rep) {
-          console.log(el);
           mes.innerHTML = 'Vous avez réussi  | QI Élevé';
           n = 0;
           button.style.display = 'none';
-          break
+          break;
         }
         
       } 
